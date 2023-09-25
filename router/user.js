2fc5c441cb474fb8b815e3526025aaf3851a2b94
黄伟杰 <hwj@cksoft.tech>
2023-09-25 18:01:00 +0800

    perf：登录接口添加字段格式校验

--- router/user.js
+++ router/user.js
@@ -11,6 +11,6 @@ const { reg_login_schema } = require('../schema/user')
 router.post('/register', expressJoi(reg_login_schema), user_handle.register)
 
 //登录
-router.post('/login', user_handle.login)
+router.post('/login', expressJoi(reg_login_schema), user_handle.login)
 
 module.exports = router
