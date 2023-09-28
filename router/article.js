1ec5bc08a00b5b27e9710108eed4881f9a889657
黄伟杰 <hwj@cksoft.tech>
2023-09-28 18:09:46 +0800

    fix：修改引用路径

--- router/article.js
+++ router/article.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const router = express.Router()
 
-const article_handler = require('../router_handler/article')
+const article_handler = require('../router_handle/article')
 // 发布文章的路由
 router.post('/add', article_handler.addArticle)
 
